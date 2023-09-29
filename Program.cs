ec87ddc8939b2636ac8640b071240347a122dfd5
Anastasia <Kirsanovasy@gmail.com>
2023-09-29 11:29:10 +0300

    third

--- Program.cs
+++ Program.cs
@@ -43,5 +43,95 @@ int[] filterEvens(int[] array)
     return result;
 }
 
+string arrayToString(int[] array)
+{
+    return "{" + String.Join(", ", array) + "}";
+}
+
+void startInput()
+{
+    Console.WriteLine($"Введите целое число что бы добавить элемент в массив {arrayToString(inputBuffer)}");
+    Console.WriteLine("Что бы выйти из режима ввода массива нажмите Enter");
+    while (true)
+    {
+        string? nextInput = Console.ReadLine();
+        if (nextInput == "")
+        {
+            break;
+        }
+        int nextNumber;
+        try
+        {
+            nextNumber = int.Parse(nextInput!.Replace(" ", ""));
+        }
+        catch (ArgumentNullException)
+        {
+            Console.WriteLine($"Извините, не могу распознать число {nextInput}");
+            continue;
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine($"Извините, не могу распознать число {nextInput} ошибка формата");
+            continue;
+        }
+        catch (OverflowException)
+        {
+            Console.WriteLine($"Извините, число {nextInput} выходит за рамки интервала {int.MinValue} .. {int.MaxValue}");
+            continue;
+        }
+        Array.Resize(ref inputBuffer, inputBuffer.Length + 1);
+        inputBuffer[inputBuffer.Length - 1] = nextNumber;
+    }
+}
+
+void finishInput()
+{
+    Console.WriteLine("Входные данные");
+    Console.WriteLine(arrayToString(inputBuffer));
+    int[] resultArray = filterEvens(inputBuffer);
+    Console.WriteLine("Выходные данные");
+    Console.WriteLine(arrayToString(resultArray));
+}
+
+void clearInputArray()
+{
+    inputBuffer = new int[]{};
+}
+
+int getRandomArrayLengthFromInput()
+{
+    int result = 10;
+    Console.WriteLine("Введите длину случайного массива целых чисел");
+    while (true)
+    {
+        string? inputString = Console.ReadLine();
+        try
+        {
+            result = int.Parse(inputString!.Replace(" ", ""));
+            if (result <= 0)
+            {
+                Console.WriteLine($"К сожалению, длина массива должна быть положительным числом, больше нуля");
+                continue;
+            }
+            break;
+        }
+        catch (ArgumentNullException)
+        {
+            Console.WriteLine($"Извините, не могу распознать число {inputString}");
+            continue;
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine($"Извините, не могу распознать число {inputString} ошибка формата");
+            continue;
+        }
+        catch (OverflowException)
+        {
+            Console.WriteLine($"Извините, число {inputString} выходит за рамки интервала {int.MinValue} .. {int.MaxValue}");
+            continue;
+        }
+    }
+    return result;
+}
 
 
